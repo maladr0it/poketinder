725378f0b9b28a5d3a2fb2eb36265182df78247a
maladr0it <bradford.lachlan@gmail.com>
2017-09-15 11:27:54 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+// app is a stack of 'cards' that is worked through.
+class App extends Component {
+  render() {
+    return (
+      <div>
+        hi
+      </div>
+    );
+  }
+}
+export default App;
